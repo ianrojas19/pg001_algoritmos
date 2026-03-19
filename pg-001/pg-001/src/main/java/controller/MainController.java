dbae6196aaeac98b0247998f9308e19475099fdc
Konny <konnyaguero@gmail.com>
2026-03-19 15:00:11 -0600

    correccion maincontroller

--- pg-001/pg-001/src/main/java/controller/MainController.java
+++ pg-001/pg-001/src/main/java/controller/MainController.java
@@ -4,7 +4,10 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.Initializable;
 import javafx.scene.canvas.Canvas;
-import javafx.scene.control.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.Slider;
 import model.Recursion;
 import model.RecursionEngine;
 import model.TreePainter;
@@ -15,8 +18,13 @@ import java.util.ResourceBundle;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class MainController implements Initializable {
+
     @javafx.fxml.FXML
     private Canvas canvasTree;
+    @javafx.fxml.FXML
+    private Slider sliderFactN;
+    @javafx.fxml.FXML
+    private Label lblFactN;
     @javafx.fxml.FXML
     private Button btnFactReset;
     @javafx.fxml.FXML
@@ -24,44 +32,39 @@ public class MainController implements Initializable {
     @javafx.fxml.FXML
     private Label lblComplexity;
     @javafx.fxml.FXML
-    private Slider sliderFactN;
-    @javafx.fxml.FXML
-    private ToggleGroup ops_facfib;
-    @javafx.fxml.FXML
     private Label lblFactCalls;
     @javafx.fxml.FXML
-    private Label lblFactN;
-    @javafx.fxml.FXML
-    private ListView<String> listSteps;
-    @javafx.fxml.FXML
     private Label lblFactResult;
+    @javafx.fxml.FXML
+    private ListView<String> listSteps; //lista de pasos recursivos
 
-//    atributos internos de la clase controller
+    //atributos internos de la clase controller
     private final RecursionEngine engine = new RecursionEngine();
     private final TreePainter painter = new TreePainter();
     private RecursionEngine.CallNode lastRoot;
     private List<RecursionEngine.CallNode> factBFS;
 
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        setupFacTab();
+        setupFactTab();
     }
 
-    private void setupFacTab() {
-        sliderFactN.setMin(1);
-        sliderFactN.setMax(12);
-        sliderFactN.setValue(5);
-        sliderFactN.setMajorTickUnit(1);
-        sliderFactN.setSnapToTicks(true);
-        sliderFactN.valueProperty().addListener((
-                observableValue,
-                oldValue,
-                newValue) -> {
+    private void setupFactTab() {
+        sliderFactN.setMin(1); sliderFactN.setMax(12); sliderFactN.setValue(5);
+        sliderFactN.setMajorTickUnit(1); sliderFactN.setSnapToTicks(true);
+        sliderFactN.valueProperty().addListener((observable, oldValue, newValue) -> {
             lblFactN.setText(String.valueOf(newValue.intValue()));
         });
-
-        btnFactCalc.setOnAction(e -> runFactorial());
+        btnFactCalc.setOnAction(event -> runFactorial());
         btnFactReset.setOnAction(e -> resetFactTab());
+    }
+
+    private void resetFactTab() {
+        lblFactResult.setText("-");
+        lblFactCalls.setText("-");
+        lblComplexity.setText("-");
+        listSteps.getItems().clear();
     }
 
     private void runFactorial() {
@@ -73,26 +76,20 @@ public class MainController implements Initializable {
 
         engine.computeFactorial(n);
         lastRoot = engine.getTreeRoot();
-//                var chart parte graficos,
+        factBFS = TreePainter.collectBFS(lastRoot);
 
+        //llenamos la lista de pasos
         ObservableList<String> items = FXCollections.observableArrayList();
-
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < engine.getSteps().size(); i++) {
             RecursionEngine.Step step = engine.getSteps().get(i);
-            items.add(String.format("[%02d] %s", i + 1, step.description));
+            items.add(String.format("[%02d] %s", i+1, step.description));
         }
-
         listSteps.setItems(items); //setteamos la lista de pasos recursivos
         lblFactResult.setText(util.Utility.format(engine.getTreeRoot().result));
-        lblFactCalls.setText(util.Utility.format(engine.getCallCount()));
+        lblFactCalls.setText(String.valueOf(engine.getCallCount()));
         lblComplexity.setText("O(n) = O(" + n + ") llamadas");
-        painter.paint(canvasTree, lastRoot, factBFS.size(), factBFS);
-    }
 
-    private void resetFactTab() {
-        lblFactResult.setText("-");
-        lblFactCalls.setText("-");
-        lblComplexity.setText("-");
-        listSteps.getItems().clear();
+        //dibujamos el árbol de llamadas en el canva
+        painter.paint(canvasTree, lastRoot, factBFS.size(), factBFS);
     }
 }
